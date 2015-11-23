bee5e5cc8212ae9cc1b1db8143b1114fb321c7d6
Jan David <jandavid@awesometechnology.de>
2015-11-23 21:47:04 +0100

    Implement logger
    A simple logger has been implemented providing a clean API that can be
    used to log arbitrary strings.

--- lib/ai_games/logger.rb
+++ lib/ai_games/logger.rb
@@ -1,7 +1,74 @@
 require 'ai_games/logger/version'
 
 module AIGames
+  # The Logger is a simple logging facility that is indented to be used by bots
+  # in any competition by The AI Games. It provides four log levels (DEBUG,
+  # INFO, WARN and ERROR) and writes the logs to STDERR, where they are picked
+  # up by The AI Games' game engines.
   module Logger
-    # Your code goes here...
+    DEBUG = 0
+    INFO = 1
+    WARN = 2
+    ERROR = 3
+
+    class << self
+
+      # Sets the output stream the logger uses. For tests, pass StringIO.new. If
+      # you do not initialize the output stream, $stderr is used.
+      def output=(output)
+        @output = output
+      end
+
+      # Returns the log level. If the log level has not been set, the default
+      # log level is returned.
+      def log_level
+        @log_level ||= WARN
+      end
+
+      # Sets the log level. Available options are DEBUG, INFO, WARN,ERROR.
+      def log_level=(log_level)
+        if [DEBUG, INFO, WARN, ERROR].include?(log_level)
+          @log_level = log_level
+        end
+      end
+
+      # Returns true if the log level is currently set to DEBUG. Use this before
+      # calling `Logger.debug(log)` to prevent the unnecessary (and costly)
+      # creation of strings.
+      def debug?
+        DEBUG == log_level
+      end
+
+      # Prints a debug log. If the log level is not set to debug, no action is
+      # taken.
+      def debug(log)
+        output.puts log if log_level <= DEBUG
+      end
+
+      # Prints an info log. If the log level is not set to info or lower, no
+      # action is taken.
+      def info(log)
+        output.puts log if log_level <= INFO
+      end
+
+      # Prints a warn log. If the log level is not set to warn or lower, no
+      # action is taken.
+      def warn(log)
+        output.puts log if log_level <= WARN
+      end
+
+      # Prints an error log.
+      def error(log)
+        output.puts log if log_level <= ERROR
+      end
+
+      private
+
+      # Returns the output stream. If the logger has not been initializes with
+      # an output stream, $stderr is returned.
+      def output
+        @output ||= $stderr
+      end
+    end
   end
 end
--- test/ai_games/logger_test.rb
+++ test/ai_games/logger_test.rb
@@ -2,8 +2,88 @@ require 'test_helper'
 
 module AIGames
   class LoggerTest < Minitest::Test
+    def setup
+      @output = StringIO.new
+      Logger.output = @output
+    end
+
     def test_that_it_has_a_version_number
       refute_nil ::AIGames::Logger::VERSION
+    end
+
+    def test_debug?
+      Logger.log_level = Logger::DEBUG
+      assert Logger.debug?
+
+      Logger.log_level = Logger::INFO
+      refute Logger.debug?
+
+      Logger.log_level = Logger::WARN
+      refute Logger.debug?
+
+      Logger.log_level = Logger::ERROR
+      refute Logger.debug?
+    end
+
+    def test_debug
+      Logger.log_level = Logger::DEBUG
+
+      Logger.debug('Tests are awesome')
+      assert_equal('Tests are awesome', output)
+
+      Logger.info('Tests are epic')
+      assert_equal('Tests are epic', output)
+
+      Logger.warn('Tests are marvelous')
+      assert_equal('Tests are marvelous', output)
+
+      Logger.error('Tests are lifesaver')
+      assert_equal('Tests are lifesaver', output)
+    end
+
+    def test_info
+      Logger.log_level = Logger::INFO
+
+      Logger.debug('Tests are awesome')
+      assert_equal(nil, output)
+
+      Logger.info('Tests are epic')
+      assert_equal('Tests are epic', output)
+
+      Logger.warn('Tests are marvelous')
+      assert_equal('Tests are marvelous', output)
+
+      Logger.error('Tests are lifesaver')
+      assert_equal('Tests are lifesaver', output)
+    end
+
+    def test_warn
+      Logger.log_level = Logger::WARN
+
+      Logger.info('Tests are epic')
+      assert_equal(nil, output)
+
+      Logger.warn('Tests are marvelous')
+      assert_equal('Tests are marvelous', output)
+
+      Logger.error('Tests are lifesaver')
+      assert_equal('Tests are lifesaver', output)
+    end
+
+    def test_error
+      Logger.log_level = Logger::ERROR
+
+      Logger.warn('Tests are marvelous')
+      assert_equal(nil, output)
+
+      Logger.error('Tests are lifesaver')
+      assert_equal('Tests are lifesaver', output)
+    end
+
+    private
+
+    def output
+      @output.string.split("\n").last
     end
   end
 end
